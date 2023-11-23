3d565a23b55f5170ec8d4cfa24fcda02d04ce5b4
ho <holee1213@naver.com>
2023-11-23 19:04:20 +0900

    Merge remote-tracking branch 'origin/Eum' into develop
    # Conflicts:
    #	src/component/Register/ProductCategory.jsx

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ProductRegister from './pages/ProductRegister';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    // errorElement: <NotFound />,
+    children: [
+      // { index : true ,path: '/', element: <App /> },
+      { path: '/products/new/:id', element: <ProductRegister /> },
+
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
