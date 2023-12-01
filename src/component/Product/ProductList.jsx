ee16b09e1dba6260ea008f8710a18f9c6f4d2f8e
tpdud0544@gmail.com <tpdud0544@gmail.com>
2023-12-01 17:12:00 +0900

    메인페이지 수정

--- src/component/Product/ProductList.jsx
+++ src/component/Product/ProductList.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import '../../style/Product/product.css';
 import Image from "../common/Image";
 
+
 export default function ProductList({image, name, price, date}){
+
+
   return(
-    <div className="product">
+    <div className="productlist">
           <div className="pro">
             <Image
               className="pro_img"
               url={image} />
             <div className="pro_comtent">
-              <p className="pro_name">
+              <p className="pro_name" >
                 {name}
               </p>
               <div className="pro_pr_da">
