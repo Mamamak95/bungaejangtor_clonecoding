4bc256aab4acbf4a16e037c6baed7e9180614997
ygy93 <dev.voalffl93@gmail.com>
2023-12-06 18:22:21 +0900

    header, footer, 검색창, 카테고리 from yang2
    header, footer 너비조절,
    검색창, 카테고리 component 로 연결
    
    2023-12-06

---
+++ src/component/header/HeaderSearch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../../style/header/headersearch.css';
+
+export default function HeaderSearch(prop){
+
+  return(
+    <div className="Searchpop">
+      ddddddd
+    </div>
+  );
+}
