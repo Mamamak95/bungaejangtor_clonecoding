736f91255a5032130468d1d6499f29a1f916a798
ygy93 <dev.voalffl93@gmail.com>
2023-12-13 15:18:55 +0900

    header, category, headersearch, searchproduct from yang2
    headerjsx 일부 경로 수정,
    categoryjsx axiox 경로를 고유경로로 수정,
    headersearchjsx 검색어 입력시 검색창 화면전환 및 검색시 검색페이지 이동
    searchproductjsx 검색한 쿼리를 가져와서 그에 해당하는 데이터 출력
    
    2023-12-13

--- src/component/header/HeaderSearch.jsx
+++ src/component/header/HeaderSearch.jsx
@@ -1,52 +1,159 @@
 import React, { useEffect, useRef, useState } from "react";
 import '../../style/header/headersearch.css';
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function HeaderSearch(){
+  const [inputFocus, setInputFocus] = useState(false); // 검색 리스트 렌더링용 (true이면 보여준다)
+  const [closeBtn, setCloseBtn] = useState(false);
+  const [searchInput, setSearchInput] = useState({value:''});
+  const [searchContent, setSearchContent] = useState(false);
+  const [searchContentChange, setSearchContentChange] = useState(false);
+  const [searchDelete, setSearchDelete] = useState(false);
 
-let searchRef = useRef(null);
-// 검색 리스트 렌더링용 (true이면 보여준다)
-const [inputFocus, setInputFocus] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-useEffect(() => {
-  function handleOutside(e) {
-    // current.contains(e.target) : 컴포넌트 특정 영역 외 클릭 감지를 위해 사용
-    if (searchRef.current && !searchRef.current.contains(e.target)) {
-      setInputFocus(false);
+  let searchRef = useRef(null);
+  let searchInputRef = useRef(null);
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    setInputFocus(false)
+
+    if(searchInput.value.trim() === ''){
+      return;
+    } else {
+      navigate(`/search?query=${searchInput.value}`);
     }
+
   }
-  document.addEventListener("mousedown", handleOutside);
-  return () => {
-    document.removeEventListener("mousedown", handleOutside);
-  };
-}, [searchRef]);
+
+  const handleSearchContent = (e) => { // Change
+    setSearchContent(false);
+    setSearchContentChange(true);
+    setSearchDelete(false);
+  }
+
+  useEffect(() => {
+    function handleOutside(e) {
+      // current.contains(e.target) : 컴포넌트 특정 영역 외 클릭 감지를 위해 사용
+      if (searchRef.current && !searchRef.current.contains(e.target)) {
+        setInputFocus(false);
+      }
+    }
+    document.addEventListener("mousedown", handleOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleOutside);
+    };
+  }, [searchRef]);
+
+  useEffect(() => {
+    if (searchInput.value !== '') {
+      setCloseBtn(true);
+    } else {
+      setCloseBtn(false);
+    }
+  }, [searchInput.value]);
+
+  
+  useEffect(() => {
+
+    if(location.pathname === `/search`){
+      setSearchInput((searchInputRef.current.value))
+    } else {
+      setSearchInput({value:''})
+      setSearchContentChange(false)
+      setSearchContent(false)
+    }
+  }, [location.pathname])
+
+  const handleSearchClose = (e) => {
+    setInputFocus(!inputFocus)
+  }
+
+  const handleChangeSearch = (e) => {
+    const { value } = e.target;
+    setSearchInput({...searchInput, value})
+
+    if(value.length < 26){
+      setSearchInput({...searchInput, value})
+
+      setSearchContent(true)
+      setSearchContentChange(true)
+
+      if(e.target.value === ''){
+        setSearchContent(false)
+        setSearchContentChange(false)
+      }
+    }
+    
+    console.log(location.pathname);
+  }
+
+  const handleSearchValueDelete = (e) => {
+    setCloseBtn(false)
+    setSearchInput({value:''})
+    searchInputRef.current.focus();
+  }
 
   return(
     <>
-      <form className="HeaderSearch" ref={searchRef} onFocus={() => {setInputFocus(true)}}>{/* 헤더메인부분의 검색창을 다루는 컴포넌트 */}
+      <form className="HeaderSearch" 
+        ref={searchRef} 
+        onFocus={() => {setInputFocus(true)}}
+        onSubmit={handleSearchSubmit}>
           { inputFocus &&
             <div className="Searchpop" id="searchpop">
-            <div className="searchcontent">
-              <div className="titlename">
-                <div className="recently">최근 검색어</div>
-                <div className="popular">인기 검색어</div>
+              <div className="searchcontent" onChange={handleSearchContent}>
+              { !searchContent &&
+                <>
+                  <div className="titlename">
+                    <div className="recently">최근 검색어</div>
+                    <div className="popular">인기 검색어</div>
+                  </div>
+                  <div className="searchcenter">
+                    <img src="https://m.bunjang.co.kr/pc-static/resource/fb38b8548f0c80b100ad.png" alt="searchcenterImg" />
+                    <p>최근 검색어가 없습니다.</p>
+                  </div>
+                </>
+              }
+              {
+                searchContentChange &&
+                <>
+                  <div className="titlename2">
+                    <i class="fa-solid fa-shop searchproducticon"></i>
+                    <div className="recently2">상품검색 > <span>{searchInput.value}</span></div>
+                    <div className="popular2"> 상품명으로 검색</div>
+                  </div>
+                  <div className="searchcenter2">
+                    <p>{searchInput.value}</p>
+                  </div>
+                </>
+              }
               </div>
-              <div className="searchcenter">
-                <img src="https://m.bunjang.co.kr/pc-static/resource/fb38b8548f0c80b100ad.png" alt="searchcenterImg" />
-                <p>최근 검색어가 없습니다.</p>
+              <div className="searchfooter">
+                { !searchDelete && <div className="searchdelete"><i class="fa-solid fa-trash-can"></i> 검색어 전체삭제</div> }
+                { searchDelete && <div></div> }
+                <div className="searchclose" onClick={handleSearchClose}>닫기</div>
               </div>
             </div>
-            <div className="searchfooter">
-              <div className="searchdelete"><i class="fa-solid fa-trash-can"></i> 검색어 전체삭제</div>
-              <div className="searchclose">닫기</div>
-            </div>
-          </div>
           }
 
         <div className="search">
-          <input placeholder="상품명,지역명,@상점명 입력" type="text" />
+          <input className="searchinput" 
+            value={searchInput.value} 
+            ref={searchInputRef} 
+            onChange={handleChangeSearch}
+            placeholder="상품명,지역명,@상점명 입력" type="text" />
         </div>
-        <button type="button" className="searchCloseBtn"><img src="https://m.bunjang.co.kr/pc-static/resource/8221ab3198c73f8141a4.png" alt="searchCloseBtn" /></button>
-        <img src="https://m.bunjang.co.kr/pc-static/resource/2be3c66fa47ccd5ece2a.png" alt="searchImg" />
+        { closeBtn &&
+          <button type="button" className="searchCloseBtn" onClick={handleSearchValueDelete}>
+            <img src="https://m.bunjang.co.kr/pc-static/resource/8221ab3198c73f8141a4.png" alt="searchCloseBtn" />
+          </button>
+        }
+        <button>
+          <img src="https://m.bunjang.co.kr/pc-static/resource/2be3c66fa47ccd5ece2a.png" alt="searchImg" />
+        </button>
       </form>
     </>
   );
---
+++ src/pages/SearchProduct.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import '../style/searchproduct/searchproduct.css';
+import { useLocation, Link } from 'react-router-dom';
+import formatRelativeDate from "../util/date.js";
+import Image from "../component/common/Image";
+import axios from 'axios';
+
+export default function SearchProduct(prop){
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const query = queryParams.get("query");
+
+  const [searchNameList, setSearchNameList] = useState([]);
+  const [searchCount, setSearchCount] = useState('');
+  const [sortBy, setSortBy] = useState('date');
+
+  useEffect(() => {
+    axios
+    .get(`http://127.0.0.1:8000/search?query=${query}`)
+    .then(data => {
+      setSearchNameList(data.data)
+      setSearchCount(data.data.length)
+      console.log(data.data);
+    })
+    .catch(error => {
+      console.error("에러 발생:", error);
+    });
+  },[query])
+
+  const handleSort = (sequence) => {
+    let sortList = [...searchNameList];
+    
+    if (sequence === 'date') {
+      sortList.sort((a, b) => new Date(b.regdate) - new Date(a.regdate));
+    } else if (sequence === 'registration') {
+      sortList.sort((a, b) => new Date(a.regdate) - new Date(b.regdate));
+    }
+
+    setSearchNameList(sortList);
+    setSortBy(sequence);
+  };
+
+  return(
+    <>
+      <div className="searchProduct inner">
+        <div className="searchproductnav">
+          <h2 className="searchproducttitle">
+            <div className="searchproductname">{query}</div>
+            의 검색결과
+            <div className="searchproductcount">{searchCount}개</div>
+          </h2>
+
+          <ul className="searchsequence">
+            <li className={`searchsequence1 ${sortBy === 'date' ? 'selected' : ''}`} onClick={() => handleSort('date')}>
+              최신순
+            </li>
+            <li className={`searchsequence2 ${sortBy === 'registration' ? 'selected' : ''}`} onClick={() => handleSort('registration')}>
+              등록순
+            </li>
+          </ul>
+        </div>
+
+        <div className="searchproductmain">
+          <ul className="searchproductcontent">
+            {
+              searchNameList.map(list => 
+                <li>
+                  <Link to = {`/productDetail/${list.pid}`} className="searchlistlink">
+                    <Image className="pro_img" url={list.img} />
+                    <p>{list.productName}</p>
+                    <div className="searchlistinfo">
+                      <div>{list.price} 원</div>
+                      <div>{formatRelativeDate(list.regdate)}</div>
+                    </div>
+                  </Link>
+                </li>
+              )
+            }
+          </ul>
+        </div>
+
+        <div className="searchpage">
+          <ul className="searchpagenation">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
